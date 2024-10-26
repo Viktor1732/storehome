fc2912d67d6f3bc2e489f6c0e43864a726f9b45c
Viktor R. <programmer.kras@gmail.com>
2024-10-26 11:56:13 +0700

    added and configured templates base.html, about.html, added function about

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import index
+from main.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('about/', about, name='about'),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
+
 def index(request):
-    return render(request, 'main/index.html')
+    context = {"title": "StoreHome - Главная", "content": "Магазин мебелеи StoreHome"}
+
+    return render(request, "main/index.html", context=context)
+
+
+def about(request):
+    context = {
+        'title': 'StoreHome - О Нас!',
+        'content': 'StoreHome - Магазин для Всех!',
+        'text_on_page': 'Наш магазин действительно хорош! Мы крутые ребята!'
+    }
+
+    return render(request, 'main/about.html', context=context)
