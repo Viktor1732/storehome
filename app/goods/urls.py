e2a474f07b17839ff72fdf29e935c6ee1dd3b3f2
Viktor R. <programmer.kras@gmail.com>
2024-10-26 19:20:06 +0700

    added app 'goods', made initial settings. added templates: catalog, product

---
+++ app/goods/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from goods.views import catalog, product
+
+
+app_name = 'goods'
+
+urlpatterns = [
+    path('', catalog, name='catalog'),
+    path('product/', product, name='product'),
+]
---
+++ app/goods/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def catalog(request):
+    context = {
+        'title': 'StoreHome - Каталог',
+    }
+    return render(request, 'goods/catalog.html', context=context)
+
+
+def product(request):
+    context = {
+        'title': 'StoreHome - Каталог',
+    }
+    return render(request, 'goods/product.html', context=context)
