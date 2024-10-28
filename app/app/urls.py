e11859d74973c44f1630d4dee79b031cc6bed1e6
Viktor R. <programmer.kras@gmail.com>
2024-10-28 16:03:34 +0700

    configured the media files.

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from app.settings import DEBUG
 
 
@@ -30,3 +32,4 @@ if DEBUG:
     urlpatterns += [
         path("__debag__/", include("debug_toolbar.urls")),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
