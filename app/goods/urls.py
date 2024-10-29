f5419b4337361c6bb11a12971711036d13698b44
Viktor R. <programmer.kras@gmail.com>
2024-10-29 20:23:01 +0700

    added display of urls for products using slug

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from goods.views import catalog, product
 
 
-app_name = 'goods'
+app_name = "goods"
 
 urlpatterns = [
-    path('', catalog, name='catalog'),
-    path('product/', product, name='product'),
+    path("", catalog, name="catalog"),
+    path("product/<slug:product_slug>/", product, name="product"),
 ]
--- app/goods/views.py
+++ app/goods/views.py
@@ -3,14 +3,18 @@ from goods.models import Products
 
 
 def catalog(request):
+    goods = Products.objects.all()
     context = {
         "title": "StoreHome - Каталог",
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context=context)
 
 
-def product(request):
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
     context = {
         "title": "StoreHome - Каталог",
+        "product": product,
     }
     return render(request, "goods/product.html", context=context)
