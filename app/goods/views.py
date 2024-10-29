5c18a6e7c443e462f5982ca240e8f1503712a9a6
Viktor R. <programmer.kras@gmail.com>
2024-10-29 14:56:46 +0700

    simple tag connected for categories

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from goods.models import Products
 
-def catalog(request):
-    goods = Products.objects.all()
 
+def catalog(request):
     context = {
         "title": "StoreHome - Каталог",
-        "goods": goods}
+    }
     return render(request, "goods/catalog.html", context=context)
 
 
