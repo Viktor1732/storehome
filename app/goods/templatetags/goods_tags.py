3fd7b30e0aa2500c0ef50f0347a48d1e14d01be5
Viktor R. <programmer.kras@gmail.com>
2024-10-31 21:53:25 +0700

    the product filter on the site page and urlencode

--- app/goods/templatetags/goods_tags.py
+++ app/goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from django.utils.http import urlencode
 from django import template
 from goods.models import Categories
 
@@ -8,3 +9,14 @@ register = template.Library()
 @register.simple_tag()
 def categories_tag():
     return Categories.objects.all()
+
+
+# takes_context=True, означает, что все контекстные переменные из context (включая request) попадут в параметр: context функции chage_params
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    # Формируем словарь
+    query = context["request"].GET.dict()
+    # Расширяем словарь, тем что можем передать из шаблона
+    query.update(kwargs)
+    #urlencode - формирует готовую строку, которую можно использовать в url-адресе
+    return urlencode(query)
