48d48aa32c78962cb0df8377990c4c08c0b10bc9
Viktor R. <programmer.kras@gmail.com>
2024-11-02 23:22:42 +0700

    performed preliminary debugging of the code.

--- app/main/views.py
+++ app/main/views.py
@@ -5,19 +5,19 @@ from goods.models import Categories
 def index(request):
     categories = Categories.objects.all()
     context = {
-        "title": "StoreHome - Главная", 
+        "title": "StoreHome - Главная",
         "content": "Магазин мебелеи StoreHome",
-        'categories': categories
-        }
+        "categories": categories,
+    }
 
     return render(request, "main/index.html", context=context)
 
 
 def about(request):
     context = {
-        'title': 'StoreHome - О Нас!',
-        'content': 'StoreHome - Магазин для Всех!',
-        'text_on_page': 'Наш магазин действительно хорош! Мы крутые ребята!'
+        "title": "StoreHome - О Нас!",
+        "content": "StoreHome - Магазин для Всех!",
+        "text_on_page": "Наш магазин действительно хорош! Мы крутые ребята!",
     }
 
-    return render(request, 'main/about.html', context=context)
+    return render(request, "main/about.html", context=context)
