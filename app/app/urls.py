78c5769dd03529f959c68b22644599be53fc6fa0
Viktor R. <programmer.kras@gmail.com>
2024-11-16 20:16:34 +0700

    created orders app, added template and urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path("", include("main.urls", namespace="main")),
     path("catalog/", include("goods.urls", namespace="goods")),
     path("users/", include("users.urls", namespace="user")),
-    path("casts/", include("carts.urls", namespace="cart"))
+    path("casts/", include("carts.urls", namespace="cart")),
+    path("orders/", include("orders.urls", namespace="orders")),
 ]
 
 if DEBUG:
