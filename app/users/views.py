f013ddf4ce3e7c1ff1b36be06b5223a2aa958d26
Viktor R. <programmer.kras@gmail.com>
2024-11-17 22:42:39 +0700

    configured the display of completed orders to profile

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from orders.models import Order, OrderItem
 from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
@@ -83,9 +85,19 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user)
 
+    orders = (
+        Order.objects.filter(user=request.user).prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        ).order_by("-id")
+    )
+
     context = {
         "title": "StoreHome - Profile",
         "form": form,
+        "orders": orders,
     }
     return render(request, "users/profile.html", context=context)
 
